d708bc8335002683f81a9be79b84ee65b802f7fa
Daniele Cesca <danielecesca@Danieles-MacBook-Air.local>
2022-04-29 13:45:03 +0200

    add global styles

---
+++ client/src/GlobalStyles.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+
+:root {
+    --color-black: #444444;
+    --color-white:#ffffff;
+    --color-light-gray: #ebebeb;
+    --color-green: #084d2c;
+    --color-red: #ff501c;
+    --color-blue: #153eff;
+    --border-color:#444444;
+    --border-radius: 5px;
+    --box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  }
+
+*{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+body{
+    max-width: 768px;
+    height: 100vh;
+    margin: 0 auto;
+    padding: 0;
+    font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
+    color: var(--color-white);
+    background-color: var(--color-black);
+}
+
+input, label, textarea{
+    font-size: 1em;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0 auto;
+}
+`;
